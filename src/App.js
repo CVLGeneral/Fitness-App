a71b90dfd5b71ed078c84e6bae3b7044c108fa94
lewisky <lewisky28mawira@gmail.com>
2023-05-03 10:12:09 +0300

    completed BodyFeature component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+import BodyFeature from "./components/BodyFeature";
+import { Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+import ExerciseDetail from "./components/Detail";
 import Loader from "../src/components/Loader";
-import { Routes, Route } from 'react-router-dom';
-
-
-
-//make sure you add your imports
-
-
-
-function App() {
+const App = () => {
   const [loaded, setLoaded] = useState(true);
 
   useEffect(() => {
@@ -18,28 +16,25 @@ function App() {
       clearTimeout(timer);
     };
   }, []);
-
   return (
-    <>
-     {loaded ? (
-       <Loader />
+    <div>
+      <BodyFeature />
+
+      {loaded ? (
+        <Loader />
       ) : (
         <Section>
           {/* <Navbar /> */}
           <Routes>
-            {/* <Route path="/" element={<Home />} />
-            <Route path={`/exercise/:id`} element={<ExerciseDetail />} />  */}
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path={`/exercise/:id`} element={<ExerciseDetail />} />
           </Routes>
           {/* <Footer /> */}
         </Section>
       )}
-
-    
-    
-    
-    </>
-
+    </div>
   );
-}
+};
+
 const Section = styled.section``;
 export default App;
